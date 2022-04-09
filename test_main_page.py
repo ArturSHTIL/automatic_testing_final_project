9c1fb0a940e7619143d3efa4b3c8de1c78ec1deb
Artur Kalenik <arturshtill@gmail.com>
2022-04-09 20:14:30 +0300

    Added changes to files: Created class TestLoginFromPage and added login_guest mark

--- test_main_page.py
+++ test_main_page.py
@@ -1,24 +1,25 @@
-import time
 from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-from .pages.exzmple_basket_page import ExampleBasketPage
+import pytest
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
 
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_for_login_or_reg(browser):
@@ -50,5 +51,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.should_be_empty_basket()
     page.should_be_text_about_empty_basket()
     page.should_not_be_empty_basket()
-
-
